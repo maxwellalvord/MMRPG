24413356fdae6916229a6cbcd2433afb7cc7acec
Maxwell Alvord <maxwellalvord@gmail.com>
2022-08-31 15:09:04 -0700

    add character state creation to index html
    Co-authored-by: Matt Herbert <matthew.f.herbert@gmail.com>

--- src/index.js
+++ src/index.js
@@ -1,12 +1,42 @@
 import $ from 'jquery';
 import welcome from "./js/salutation.js";
+import {storeState, changeState, stateControl, setName} from './js/comp.js';
+
+// let player = {name: "", health:10, PP:10};
+// let playerStats = [];
+
+// const characterGenerator = (playerInput) => {
+//   const userPlayer = storeState(player);
+//   // const userName = userPlayer(setName(playerInput));
+//   userPlayer(setName(`player${playerInput}`));
+//   playerStats.push(userPlayer);
+// }
 
 $(document).ready(function () {
-  $("#r1").submit(function (event) {
+  let player = {name: "", health:10, PP:10};
+  let playerStats = [];
+
+  const characterGenerator = (playerInput) => {
+    const userPlayer = storeState(player);
+    // const userName = userPlayer(setName(playerInput));
+    userPlayer(setName(`${playerInput}`));
+    playerStats.push(userPlayer);
+  }
+
+  $("#form").submit(function(event){
     event.preventDefault();
-    let sal = $("#sal").val();
-    let fname = $("#fname").val();
-    console.log(sal);
-    $("#greeting").append(welcome(sal)(fname));
+    let playerInput = $("#charInput").val();
+    characterGenerator(playerInput);
+    console.log(playerInput);
+    console.log(playerStats[0]());
   })
+  
+$("#r1").submit(function (event) {
+  event.preventDefault();
+  let sal = $("#sal").val();
+  let fname = $("#fname").val();
+  console.log(sal);
+  $("#greeting").append(welcome(sal)(fname));
+  console.log(playerStats);
+})
 })
---
+++ src/js/comp.js
@@ -0,0 +1,29 @@
+
+export const storeState = initialValue => {
+  let currentState = initialValue;
+  return (stateChangeFunction = state => state) => {
+    const newState = stateChangeFunction(currentState);
+    currentState = { ...newState };
+    return newState;
+  };
+};
+
+export const changeState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : (state[prop] || 0) + value
+    });
+  };
+};
+
+export const setState = (prop) => {
+  return (value) => {
+    return (state) => ({
+      ...state,
+      [prop] : value
+    });
+  };
+};
+
+export const setName = setState("name");
